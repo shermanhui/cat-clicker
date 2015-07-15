a7e3807225ea33c41647419b6ea42e0064c0393b
shui91 <sherman.sy.hui@gmail.com>
2015-07-14 23:09:57 -0700

    feat: updated to have three cats, with random name generation, and individual click tracking todo: apply DRY principle to click counting and consolidate repeated code

--- js/main.js
+++ js/main.js
@@ -1,11 +1,47 @@
-var $nyan = $('#nyan-cat');
-var $counter = $('#counter-p')
-var clickCount = 0;
+var $nyans = $('.nyan-cats');
+var $nyanOne = $('#nyan-cat-one');
+var $nyanTwo = $('#nyan-cat-two');
+var $nyanThree = $('#nyan-cat-three');
 
-console.log(clickCount);
+var $nameOne = $('#name-one');
+var $nameTwo = $('#name-two');
+var $nameThree = $('#name-three');
 
-$nyan.click(function () {
-	clickCount += 1;
-	$counter.text('Current Click Count = '+ clickCount);
+var $counter = $('#counter-one');
+var $counter2 = $('#counter-two');
+var $counter3 = $('#counter-three');
+
+var clickCountOne = 0;
+var clickCountTwo = 0;
+var clickCountThree = 0;
+
+var cats = [$nameOne, $nameTwo, $nameThree];
+var numCats = cats.length;
+var names = ['Ned', 'Jaime', 'Jon', 'Arya', 'Sansa', 'Tyrion'];
+
+console.log(clickCountOne, clickCountTwo, clickCountThree);
+
+var nameCats = function(){
+	for (var i = 0; i < numCats; i++) {
+		var catName = names[Math.floor(Math.random() * 6)]
+		var cat = cats[i];
+		cat.text(catName + ' Nyan');
+	}
+};
+
+$nyanOne.click(function () {
+	clickCountOne += 1;
+	$counter.text('Current Click Count = '+ clickCountOne);
 });
 
+$nyanTwo.click(function () {
+	clickCountTwo += 1;
+	$counter2.text('Current Click Count = '+ clickCountTwo);
+});
+
+$nyanThree.click(function () {
+	clickCountThree += 1;
+	$counter3.text('Current Click Count = '+ clickCountThree);
+});
+
+nameCats();
