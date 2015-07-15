84eac34cf42fba31d67c209e9b17c881ccad1f79
shui91 <sherman.sy.hui@gmail.com>
2015-07-15 00:20:28 -0700

    feat: encapsulated countClickers into clickTracker() which only calls .click() once, comments were also added to clarify code and add explanations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var $nyans = $('.nyan-cats');
-var $nyanOne = $('#nyan-cat-one');
-var $nyanTwo = $('#nyan-cat-two');
-var $nyanThree = $('#nyan-cat-three');
+var $nyanOne = $('#nyan-cat-1');// currently unused variables
+var $nyanTwo = $('#nyan-cat-2');// currently unused variables
+var $nyanThree = $('#nyan-cat-3');// currently unused variables
 
 var $nameOne = $('#name-one');
 var $nameTwo = $('#name-two');
@@ -19,29 +19,51 @@ var cats = [$nameOne, $nameTwo, $nameThree];
 var numCats = cats.length;
 var names = ['Ned', 'Jaime', 'Jon', 'Arya', 'Sansa', 'Tyrion'];
 
-console.log(clickCountOne, clickCountTwo, clickCountThree);
+//console.log(clickCountOne, clickCountTwo, clickCountThree);
 
+// this function randomly names the three cats using the array of GoT character names provided
+// iterates through a the number of cats and inputs the name!
 var nameCats = function(){
 	for (var i = 0; i < numCats; i++) {
-		var catName = names[Math.floor(Math.random() * 6)]
+		var catName = names[Math.floor(Math.random() * 6)];
 		var cat = cats[i];
 		cat.text(catName + ' Nyan');
 	}
 };
 
-$nyanOne.click(function () {
-	clickCountOne += 1;
-	$counter.text('Current Click Count = '+ clickCountOne);
-});
-
-$nyanTwo.click(function () {
-	clickCountTwo += 1;
-	$counter2.text('Current Click Count = '+ clickCountTwo);
-});
-
-$nyanThree.click(function () {
-	clickCountThree += 1;
-	$counter3.text('Current Click Count = '+ clickCountThree);
-});
+var clickTracker = function() {
+	// Trying to work with closures here...but it's a bit of a mess! To be completed!
+	// for (var i = 0; i <= numCats; i++){
+	// 	(function(){
+	// 		var catId = i;
+	// 		$('#nyan-cat-' + catId).click(function(){
+	// 			console.log(catId);
+	// 			var clickCount = 'clickCount' + catId;
+	// 			console.log(clickCount);
+	// 			$counter.text('Current Click Count = ' + clickCount);
+	// 		});
+	// 	})();
+	// };
+	var clickedItemId = null;
+	$nyans.click(function(){
+		// Practicing usage of "this" to grab the last object clicked, in this case the individual Nyan cat id
+		// so we can identify and increment the appropriate clicker
+		// this article helped with clarifying "this" http://javascriptissexy.com/understand-javascripts-this-with-clarity-and-master-it/
+		clickedItemId = $(this).attr('id');
+		//console.log(clickedItemId);
+		//console.log($nyanOne);
+		if (clickedItemId == "nyan-cat-1") {
+			clickCountOne ++;
+			$counter.text('Current Click Count = '+ clickCountOne);
+		} else if (clickedItemId == "nyan-cat-2") {
+			clickCountTwo ++;
+			$counter2.text('Current Click Count = '+ clickCountTwo);
+		} else {
+			clickCountThree ++;
+			$counter3.text('Current Click Count = ' + clickCountThree);
+		}
+	});
+};
 
 nameCats();
+clickTracker();
